4bd9e356cde7b44b9d96b205f46134af554f6993
Tkachenko_Yuriy <uvt86@ukr.net>
2024-04-25 15:33:06 +0300

    add hw_06

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { signup, signin, getCurrent, logout, updateAvatar } from "../controllers/authControllers.js";
+import { signup, signin, getCurrent, logout, updateAvatar, verify, resendVerify } from "../controllers/authControllers.js";
 import validateBody from "../helpers/validateBody.js";
-import { userSigninSchema, userSignupSchema } from "../schemas/userSchema.js";
+import {userEmailSchema, userSigninSchema, userSignupSchema } from "../schemas/userSchema.js";
 import authenticate from "../middlewares/authenticate.js"
 import upload from "../middlewares/upload.js";
 
@@ -16,5 +16,9 @@ authRouter.get("/current", authenticate, getCurrent);
 authRouter.post("/logout", authenticate, logout)
 
 authRouter.patch("/avatars", upload.single("avatar"), authenticate, updateAvatar)
+
+authRouter.get("/verify/:verificationToken", verify);
+
+authRouter.post("/verify", validateBody(userEmailSchema), resendVerify)
 
 export default authRouter;
